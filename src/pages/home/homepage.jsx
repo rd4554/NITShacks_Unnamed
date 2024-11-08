4db8e88675b794202619c9b9f499fc908d9d219e
Nishit Baishya <nishit@Nishits-MacBook-Air.local>
2024-11-08 19:48:12 +0530

    adddedcomponents

--- src/pages/home/homepage.jsx
+++ src/pages/home/homepage.jsx
@@ -1,19 +1,28 @@
-
+// src/pages/Home.jsx
 import React from 'react';
+import GreenWheelMeter from '../../components/greenwheel';
+import WeeklyScore from '../../components/weeklyscore';
+import LoadingBar from '../../components/progressbar';
+import Taskbox from '../../components/taskbox';
+import VerticalBoxes from '../../components/tasks';
+import GreenWheelLogo from '../../components/greenwheellogo';
 function Home() {
   return (
-    <>
-        <div className='overflow-hidden'>
-
-            <section id="one" className="h-[100vh] w-[100vw] bg-black "></section>
-
-            <section id="two" className="h-[100vh] w-[100vw] bg-black "></section>
-
-            <section id="three" className="h-[100vh] w-[100vw] bg-black "></section>
-        </div>
-    </>
-  )
+    <div className="text-gray-800">
+      {/* Center the components with some space */}
+      <div className="flex justify-center items-center flex-col space-y-8 py-20">
+        {/* Green Wheel Meter with 75% progress */}
+        <GreenWheelMeter/> 
+        <WeeklyScore/>
+        <LoadingBar/>
+        <Taskbox/>
+        <VerticalBoxes/>
+        <GreenWheelLogo/>
+    
+        {/* Weekly Score showing 4/16 */}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
-
