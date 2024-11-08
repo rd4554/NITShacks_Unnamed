0648f6762e01eec6b20a404a14511b5a0971e58e
rd4554 <reechadeka@gmail.com>
2024-11-08 19:45:16 +0530

    bg changes

--- src/pages/home/homepage.jsx
+++ src/pages/home/homepage.jsx
@@ -3,13 +3,23 @@ import React from 'react';
 function Home() {
   return (
     <>
-        <div className='overflow-hidden'>
+      <div className="overflow-hidden ">
 
-            <section id="one" className="h-[100vh] w-[100vw] bg-black "></section>
+            <section id="one" className="h-[100vh] w-[100vw] bg-black overflow-hidden bg-[#2B7150]">
+            <div
+  className="h-[85vh] w-[90vw] ml-16 mt-14 rounded-3xl"
+  style={{ background: 'linear-gradient(245.04deg, #62F35E 0.77%, #387436 99.23%)'}}></div>
 
-            <section id="two" className="h-[100vh] w-[100vw] bg-black "></section>
+            
+            </section>
 
-            <section id="three" className="h-[100vh] w-[100vw] bg-black "></section>
+            <section id="two" className="h-[100vh] w-[100vw] bg-black bg-[#2B7150]"><div
+  className="h-[85vh] w-[90vw] ml-16 mt-14 rounded-3xl"
+  style={{ background: 'linear-gradient(245.04deg, #62F35E 0.77%, #387436 99.23%)'}}></div></section>
+
+            <section id="three" className="h-[100vh] w-[100vw] bg-black bg-[#2B7150] "><div
+  className="h-[85vh] w-[90vw] ml-16 mt-14 rounded-3xl"
+  style={{ background: 'linear-gradient(245.04deg, #62F35E 0.77%, #387436 99.23%)'}}></div></section>
         </div>
     </>
   )
