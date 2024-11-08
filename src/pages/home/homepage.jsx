acb6a35d58c395486d8c4c30382f2e5db0d243ef
Nishit Baishya <nishit@Nishits-MacBook-Air.local>
2024-11-08 20:17:08 +0530

    grapg

--- src/pages/home/homepage.jsx
+++ src/pages/home/homepage.jsx
@@ -1,4 +1,3 @@
-// src/pages/Home.jsx
 import React from 'react';
 import GreenWheelMeter from '../../components/greenwheel';
 import WeeklyScore from '../../components/weeklyscore';
@@ -6,19 +5,37 @@ import LoadingBar from '../../components/progressbar';
 import Taskbox from '../../components/taskbox';
 import VerticalBoxes from '../../components/tasks';
 import GreenWheelLogo from '../../components/greenwheellogo';
+
+// Importing the BarGraph component
+import BarGraph from '../../components/bargraph';
+
 function Home() {
+  const data = [
+    { label: 'Jan', value: 100 },
+    { label: 'Feb', value: 20 },
+    { label: 'Mar', value: 50 },
+    { label: 'Apr', value: 80 },
+    { label: 'May', value: 60 },
+    { label: 'Jun', value: 90 },
+  ];
+
   return (
     <div className="text-gray-800">
       {/* Center the components with some space */}
       <div className="flex justify-center items-center flex-col space-y-8 py-20">
         {/* Green Wheel Meter with 75% progress */}
-        <GreenWheelMeter/> 
-        <WeeklyScore/>
-        <LoadingBar/>
-        <Taskbox/>
-        <VerticalBoxes/>
-        <GreenWheelLogo/>
-    
+        <GreenWheelMeter /> 
+        <WeeklyScore />
+        <LoadingBar />
+        <Taskbox />
+        <VerticalBoxes />
+        <GreenWheelLogo />
+
+        {/* Bar Graph Component */}
+        <div className="w-full max-w-lg mt-8">
+          <BarGraph data={data} />
+        </div>
+
         {/* Weekly Score showing 4/16 */}
       </div>
     </div>
