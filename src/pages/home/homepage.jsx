b0769958c6a696cffee9671d8b1ea3a5f055369d
rd4554 <reechadeka@gmail.com>
2024-11-08 19:50:27 +0530

    bg change

--- src/pages/home/homepage.jsx
+++ src/pages/home/homepage.jsx
@@ -5,7 +5,21 @@ function Home() {
     <>
       <div className="overflow-hidden ">
 
-            <section id="one" className="h-[100vh] w-[100vw] bg-black overflow-hidden bg-[#2B7150]">
+            <section id="home" className="h-[100vh] w-[100vw] overflow-hidden bg-[#2B7150]">
+            <div
+  className="h-[85vh] w-[90vw] ml-16 mt-14 rounded-3xl"
+  style={{ background: 'linear-gradient(245.04deg, #62F35E 0.77%, #387436 99.23%)'}}></div>
+
+            
+            </section>
+            <section id="tasks" className="h-[100vh] w-[100vw] overflow-hidden bg-[#2B7150]">
+            <div
+  className="h-[85vh] w-[90vw] ml-16 mt-14 rounded-3xl"
+  style={{ background: 'linear-gradient(245.04deg, #62F35E 0.77%, #387436 99.23%)'}}></div>
+
+            
+            </section>
+            <section id="outdoor" className="h-[100vh] w-[100vw] overflow-hidden bg-[#2B7150]">
             <div
   className="h-[85vh] w-[90vw] ml-16 mt-14 rounded-3xl"
   style={{ background: 'linear-gradient(245.04deg, #62F35E 0.77%, #387436 99.23%)'}}></div>
@@ -13,13 +27,6 @@ function Home() {
             
             </section>
 
-            <section id="two" className="h-[100vh] w-[100vw] bg-black bg-[#2B7150]"><div
-  className="h-[85vh] w-[90vw] ml-16 mt-14 rounded-3xl"
-  style={{ background: 'linear-gradient(245.04deg, #62F35E 0.77%, #387436 99.23%)'}}></div></section>
-
-            <section id="three" className="h-[100vh] w-[100vw] bg-black bg-[#2B7150] "><div
-  className="h-[85vh] w-[90vw] ml-16 mt-14 rounded-3xl"
-  style={{ background: 'linear-gradient(245.04deg, #62F35E 0.77%, #387436 99.23%)'}}></div></section>
         </div>
     </>
   )
